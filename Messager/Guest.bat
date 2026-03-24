952ce99bf9654bc7bfb88fbb1c4468eb5e47da40
XXDXYX <kolenicenkod@gmail.com>
2026-03-24 18:56:54 +0200

    remove tags and add bat files

---
+++ Messager/Guest.bat
@@ -0,0 +1,2 @@
+@echo off
+start "" "%~dp0Guest\build\Desktop_Qt_6_10_2_MSVC2022_64bit-Release\appGuestClient.exe"
---
+++ Messager/Server.bat
@@ -0,0 +1,2 @@
+@echo off
+start "" "%~dp0Server\build\Desktop_Qt_6_10_2_MSVC2022_64bit-Release\appServerClient.exe"
